a62c8a4f6d990de7be6b113307dee9de06b30927
jsmx2025 <JSMX_1996@wireless-s0-so-150-37-172.uchicago.edu>
2017-01-31 01:07:01 -0600

    Final
    finished to best of ability

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,23 +4,48 @@ class GamesController < ApplicationController
 def play_rock
   @computer_move = ["rock", "paper", "scissors"].sample
   if @computer_move == "rock"
-    @outcome = "tied"
+    @outcome = "tie"
   elsif @computer_move == "paper"
     @outcome = "lost"
   else
-    @outcome = "wo"
-  end
-
-
+    @outcome = "won"
   render("/games/play_rock.html.erb")
 end
 
+end
+
 def play_paper
+  @computer_move = ["rock", "paper", "scissors"].sample
+  if @computer_move == "rock"
+    @outcome = "won"
+  elsif @computer_move == "scissors"
+    @outcome = "lost"
+  else
+    @outcome = "tie"
   render("/games/play_paper.html.erb")
 end
 
+end
+
 def play_scissors
+  @computer_move = ["rock", "paper", "scissors"].sample
+  if @computer_move == "rock"
+    @outcome = "lost"
+  elsif @computer_move == "scissors"
+    @outcome = "tie"
+  else
+    @outcome = "won"
   render("/games/play_scissors.html.erb")
 end
 
+def read
+  @read = "Welcome to Rock, Paper, Scissors where you'll play a game of the same name"
+render("/games/read.html.erb")
+end
+
+@rock = "fa fa-hand-rock-o move-icon"
+@scissors = "fa fa-hand-scissors-o move-icon"
+@paper = "fa fa-hand-paper-o move-icon"
+
+end
 end
